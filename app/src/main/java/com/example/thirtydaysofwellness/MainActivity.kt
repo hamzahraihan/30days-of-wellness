25c34ac64523b597ac71a09c25349c3f5a47651c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-21 16:27:41 +0700

    add top bar, and create composable for wellness app

--- app/src/main/java/com/example/thirtydaysofwellness/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysofwellness/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirtydaysofwellness.model.WellnessRepository
 import com.example.thirtydaysofwellness.ui.theme.ThirtyDaysOfWellnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThirtyDaysOfWellnessTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WellnessApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WellnessApp(modifier: Modifier = Modifier) {
+    val wellnessData = WellnessRepository.wellnesses
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.surfaceContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface,
+                    actionIconContentColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
+                title = {
+                    Text(
+                        text = "30 Days of Wellness",
+                        style = MaterialTheme.typography.displayMedium,
+
+                        )
+                },
+            )
+        },
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.surfaceContainer
+    ) { it ->
+        WellnessList(
+            wellnesses = wellnessData,
+            modifier = Modifier.padding(it)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ThirtyDaysOfWellnessTheme {
-        Greeting("Android")
+        WellnessApp()
     }
 }
