ce9811994a24ebd15a7019e106302754be5bc9a1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-21 16:29:29 +0700

    add new data to wellness data class

--- app/src/main/java/com/example/thirtydaysofwellness/model/Wellness.kt
+++ app/src/main/java/com/example/thirtydaysofwellness/model/Wellness.kt
@@ -6,5 +6,6 @@ import androidx.annotation.StringRes
 data class Wellness(
     @StringRes val titleRes: Int,
     @StringRes val descriptionRes: Int,
+    @StringRes val dayRes: Int,
     @DrawableRes val imageRes: Int,
 )
